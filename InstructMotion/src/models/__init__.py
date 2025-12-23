06e1ab6c4aa6c6f4d9b599dae91dda8720ba9d70
dauylal <dauylal884@gmail.com>
2025-12-23 15:42:56 +0800

    delete instructmotion

--- InstructMotion/src/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .value_policy_heads import AutoModelForSeq2SeqLMWithValueHead, AutoModelForSeq2SeqLMSepValueHead, AutoModelForSeq2SeqLMSepPolicyHead
-
-SUPPORTED_ARCHITECTURES = (
-    AutoModelForSeq2SeqLMSepValueHead,
-    AutoModelForSeq2SeqLMSepPolicyHead,
-    AutoModelForSeq2SeqLMWithValueHead,
-)
